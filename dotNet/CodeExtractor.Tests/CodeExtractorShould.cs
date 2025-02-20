4769689171fb1b09cf776f026620f01433682a94
Philipp Huber <philipp.huber@helbling.ch>
2025-02-20 10:15:07 +0100

    feat: started with Roslyn code analysis

---
+++ dotNet/CodeExtractor.Tests/CodeExtractorShould.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeExtractor.Tests;
+
+[TestClass]
+[TestSubject(typeof(CodeExtractor))]
+public class CodeExtractorTestX
+{
+
+    [TestMethod]
+    public void ExtractFromFileShould()
+    {
+        var basePath = "../../..";
+        var path = Path.Combine(basePath, "ExampleClass.cs");
+        
+        Assert.IsTrue(File.Exists(path));
+
+        var resultClasses  = CodeExtractor.ExtractFromFile(path, "Class");
+        Assert.AreEqual(1, resultClasses.Keys.Count);
+        Assert.Contains("ExampleClass", resultClasses.Keys);
+
+        var resultMethods  = CodeExtractor.ExtractFromFile(path, "Method");
+        Assert.AreEqual(2, resultMethods.Keys.Count);
+        Assert.Contains("ExampleClass.MethodOne", resultMethods.Keys);
+        Assert.Contains("ExampleClass.MethodTwo", resultMethods.Keys);
+    }
+}
---
+++ dotNet/CodeExtractor.Tests/ExampleClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ExampleClass
+{
+    public void MethodOne()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    private int MethodTwo(int x)
+    {
+        return x * x;
+    }
+}
